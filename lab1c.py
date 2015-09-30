490d8c116fa8594620b78fb4ea03033c6a307b76
Vladislav Nikolaev Varadinov <vladislav.varadinov@gmail.com>
2015-09-29 17:58:31 -0700

    ICS 31 Lab

---
+++ lab1c.py
@@ -0,0 +1,26 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Hello
+>>> print ('Hello')
+Hello
+>>> print (123 + 456)
+579
+>>> print (2 ** 5)
+32
+>>> print (5*4*3*2*1)
+120
+>>> number_of_students = 356
+>>> number_of_staff = 6
+>>> number_of_staff = 16
+>>> print (number_of_students/number_of_staff)
+22.25
+>>> print(0*0)
+0
+>>> print (1/0)
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    print (1/0)
+ZeroDivisionError: division by zero
+>>> 
---
+++ lab1e.py
@@ -0,0 +1,65 @@
+# vvaradin kaiweny2  
+print ('something')
+print ('anything')
+
+
+print( 2 + 4 + 6 + 8 + 10)
+
+print ((0.75 + 0.84 + 0.61 + 0.43)/4)
+
+print (2**10)
+
+
+mass = 50
+speed = 15
+
+print ( 1/2 * mass * speed ** 2)
+
+
+wall = 'w'
+cannon = 'c'
+
+print (wall+cannon)
+print (wall + cannon + wall)
+print (wall*3+cannon+wall*3)
+print (wall+cannon*2+wall+cannon*2+wall+cannon*2+wall+cannon*2)
+print (wall*3+cannon+wall*3+cannon+wall*3+cannon+wall*3+cannon+wall)
+print (wall*4+cannon+wall*4+cannon+wall*4+cannon+wall*4+cannon+wall*4)
+
+
+test_scores = ('4325220523455023')
+a = test_scores[0]
+print(a)
+
+
+
+
+
+
+s = 'anteater'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pi = 3.14159
+make = 'Toyota'
+model = 'Camry'
+year = 2014
+ICS_majors = ['Computer Science' ,'Informatics','Computer Game Science']
+a = ((3 + 5 + 7 + 9)/4)
+
